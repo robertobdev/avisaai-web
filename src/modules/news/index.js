f19db88a8f87e7bdb109cd6414f11b77c0f0c467
Roberto Barbosa <robertojf95@gmail.com>
2019-08-26 22:13:27 -0300

    adding news list

--- src/modules/news/index.js
+++ src/modules/news/index.js
@@ -1,9 +1,52 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { Table, Divider, Tag } from 'antd';
+import Parse from 'parse';
+import { Route, Link } from 'react-router-dom';
 
 const NewsListPage = () => {
+  const News = Parse.Object.extend('News');
+  const query = new Parse.Query(News);
+  const [data, setData] = useState([]);
+  const columns = [
+    {
+      title: 'Título',
+      dataIndex: 'title',
+      key: 'title',
+    },
+    {
+      title: 'Data',
+      dataIndex: 'date',
+      key: 'date',
+    },
+    {
+      title: 'Ações',
+      key: 'action',
+      render: (news) => (
+        <span>
+          <Link to={`/panel/news/add/${news.key}`}>
+            Editar
+          </Link>
+          <Divider type="vertical" />
+          <a>Deletar</a>
+        </span>
+      ),
+    },
+  ];
+  useEffect(() => {
+    query.find().then(results => {
+      console.log(results);
+      results = results.map(({ id, attributes }) => {
+        attributes = { ...attributes };
+        attributes.key = id;
+        attributes.date = new Date(attributes.createdAt.toString()).toLocaleDateString('pt-BR');
+        return attributes;
+      })
+      setData(results);
+    });
+  }, []);
+
   return (
-    <div>Olar</div>
+    <Table columns={columns} dataSource={data} />
   )
 }
 
